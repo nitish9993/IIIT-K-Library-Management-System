bb58b61a65507c02aab94ae95d0a922daaf59dbd
Nitish Singh <nitishsingh9993@gmail.com>
2018-04-20 01:36:07 +0530

    some modification

--- src/MemberList/MemberListController.java
+++ src/MemberList/MemberListController.java
@@ -40,92 +40,92 @@ public class MemberListController implements Initializable {
 
     private ConnectDB connectDB;
     ObservableList<Member> list= FXCollections.observableArrayList();
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         initcol();
         connectDB = new ConnectDB();
         LoadData();
-        
+
         tableview.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }
-    
+
     public void deleteButtonpushed()
     {
         ObservableList<Member> selectedRows,allMembers;
         allMembers = tableview.getItems();
-        
+
         selectedRows = tableview.getSelectionModel().getSelectedItems();
-        
+
         for(Member member:selectedRows)
         {
             allMembers.remove(member);
         }
     }
-    
+
     public void initcol(){
         namecol.setCellValueFactory(new PropertyValueFactory<>("name"));
         idcol.setCellValueFactory(new PropertyValueFactory<>("id"));
         numcol.setCellValueFactory(new PropertyValueFactory<>("mobile"));
         emailcol.setCellValueFactory(new PropertyValueFactory<>("email"));
     }
-        
+    //loading data    
     public void LoadData(){
         String sql = "SELECT * FROM tbl_addmember";
         try {
             Connection conn = ConnectDB.getConnections();
             PreparedStatement pst = conn.prepareStatement(sql);
             ResultSet rs = pst.executeQuery();
-            
+
             while(rs.next()){
                 String name = rs.getString("name");
                 String id = rs.getString("id");
                 String mobile = rs.getString("mobile");
                 String email = rs.getString("email");
-                
+
                 list.add(new Member(name,id,mobile,email));
             }
         } catch (SQLException ex) {
             Logger.getLogger(MemberListController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         tableview.getItems().setAll(list);
     }
-        
+    //member function
     public static class Member{
-            
+
         private final SimpleStringProperty name;
         private final SimpleStringProperty id;
         private final SimpleStringProperty mobile;
         private final SimpleStringProperty email;
-            
+
         public Member(String name,String id, String mobile,String email){
             this.name = new SimpleStringProperty(name);
             this.id = new SimpleStringProperty(id);
             this.mobile = new SimpleStringProperty(mobile);
             this.email = new SimpleStringProperty(email);
         }
-        
+
         public String getName() {
             return name.get();
         }
 
-       
+
         public String getId() {
             return id.get();
         }
 
-        
+
         public String getMobile() {
             return mobile.get();
         }
 
-        
+
         public String getEmail() {
             return email.get();
         }
-  
+
     }
 
-   
+
 }
